63e2ccbeccd83387555f366613dcb346f955d454
Meghdad Hadidi <meghdad.1987@gmail.com>
2018-08-23 10:27:37 +0430

    finished new feature: 3d

---
+++ 3d/main.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+setCanvasSize()
+
+function setCanvasSize(){
+    canvas.width = window.innerWidth - 20 
+    canvas.height = window.innerHeight - 20
+}
+
+window.addEventListener('resize', setCanvasSize)
+
+let sky = new Sky({ frequency: 2000 });
+sky.show();
+sky.animate(10);
+
+canvas.addEventListener('click', function(){
+    sky.pause();
+});
